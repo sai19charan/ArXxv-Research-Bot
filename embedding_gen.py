e2efd34b0d60d5035b43853e7c33de3be4455bb8
eeswar55555 <eeswar55555@gmail.com>
2025-10-26 10:55:44 +0530

    Revert "Eeswar"

--- embedding_gen.py
+++ embedding_gen.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pinecone import Pinecone, ServerlessSpec
 import re
 from sentence_transformers import SentenceTransformer
-from tqdm import tqdm
+from tqdm.auto import tqdm
 import time
 import os 
 from dotenv import load_dotenv 
